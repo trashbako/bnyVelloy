f0b0423994545232fdb95c782eb13e6ff3b0134e
trashbako <ienuenu@gmail.com>
2019-05-28 00:37:09 +0900

    まあなんかいろいろなおした

--- docs/script/imgDraw.js
+++ docs/script/imgDraw.js
@@ -1,31 +1,30 @@
+var cvs = null;
+var ctx = null;
+
 window.onload = function(){
     imageDraw();
 }
 
-var cvs = document.getElementById('cvs1');
-var ctx = cvs.getContext('2d');
-
-
-//画像オブジェクト
-var img = new Image();
-img.src = "./img/v_sotai.png";
-var tyt = new Image();
-tyt.src = "./img/v_josan.png";
-
-var imageDraw = function() {
+function imageDraw() {
     var opc = document.getElementById("opc").value;
     opc = opc*0.01;
 
-    
+    cvs = document.getElementById('cvs1');
+    ctx = cvs.getContext('2d');
 
+    var img = new Image();
+    img.src = "./img/v_sotai.png";
+    var tyt = new Image();
+    tyt.src = "./img/v_josan.png";
+
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.globalAlpha = 1;
+    ctx.clearRect(0,0,600,800);
     img.onload = function(){
         ctx.drawImage(img, 0, 0, 600, 800);
-        ctx.save();
         ctx.globalCompositeOperation = 'multiply';
         ctx.globalAlpha = opc;
-        tyt.onload = function(){
-            ctx.drawImage(tyt, 0, 0, 600, 800);
-        }
+        ctx.drawImage(tyt, 0, 0, 600, 800);
     }
     
 }
