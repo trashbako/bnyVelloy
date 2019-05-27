2e6d9e0ecbbddd71a058f5d5d3e207c96b6259be
trashbako <ienuenu@gmail.com>
2019-05-27 22:59:06 +0900

    taitu

--- docs/script/imgDraw.js
+++ docs/script/imgDraw.js
@@ -3,19 +3,22 @@ window.onload = function(){
 }
 
 function imageDraw() {
-    //2Dコンテキストのオブジェクトを生成する
+    
     var cvs = document.getElementById('cvs1');
     var ctx = cvs.getContext('2d');
   
-    //画像オブジェクトを生成
+    //画像オブジェクト
     var img = new Image();
     img.src = "./img/v_sotai.png";
     var tyt = new Image();
     tyt.src = "./img/v_josan.png";
   
-    //画像をcanvasに設定
+
     img.onload = function(){
         ctx.drawImage(img, 0, 0, 600, 800);
+        ctx.save();
+        ctx.globalCompositeOperation = 'multiply';
+        ctx.globalAlpha = 0.2;
         tyt.onload = function(){
             ctx.drawImage(tyt, 0, 0, 600, 800);
         }
