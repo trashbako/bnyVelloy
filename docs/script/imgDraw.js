563a71d3fa69b0bb60c0b6cd6e6b7d4d06e78d9c
trashbako <ienuenu@gmail.com>
2019-05-27 23:40:08 +0900

    taitu~

--- docs/script/imgDraw.js
+++ docs/script/imgDraw.js
@@ -6,24 +6,25 @@ var imageDraw = function() {
     
     var cvs = document.getElementById('cvs1');
     var ctx = cvs.getContext('2d');
+    ctx.clearRect(0,0,600,800);
+
     var opc = document.getElementById("opc").value;
-    opc = opc*0.1;
+    opc = opc*0.01;
     
     //画像オブジェクト
     var img = new Image();
     img.src = "./img/v_sotai.png";
     var tyt = new Image();
     tyt.src = "./img/v_josan.png";
-  
+    
 
     img.onload = function(){
-        ctx.clearRect(0,0,900,1200);
-        ctx.drawImage(img, 0, 0, 900, 1200);
+        ctx.drawImage(img, 0, 0, 600, 800);
         ctx.save();
         ctx.globalCompositeOperation = 'multiply';
         ctx.globalAlpha = opc;
         tyt.onload = function(){
-            ctx.drawImage(tyt, 0, 0, 900, 1200);
+            ctx.drawImage(tyt, 0, 0, 600, 800);
         }
     }
     
