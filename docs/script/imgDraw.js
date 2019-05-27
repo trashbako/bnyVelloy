1d19a5320bddb00c3110748432f69c6e78fe7e9f
trashbako <ienuenu@gmail.com>
2019-05-27 22:45:46 +0900

    taitu

--- docs/script/imgDraw.js
+++ docs/script/imgDraw.js
@@ -10,9 +10,19 @@ function imageDraw() {
     //画像オブジェクトを生成
     var img = new Image();
     img.src = "./img/v_sotai.png";
+    var tyt = new Image();
+    tyt.src = "./img/v_josan.png";
   
     //画像をcanvasに設定
     img.onload = function(){
-      ctx.drawImage(img, 0, 0, 600, 800);
+        ctx.drawImage(img, 0, 0, 600, 800);
+        tyt.onload = function(){
+            ctx.drawImage(tyt, 0, 0, 600, 800);
+        }
     }
-  }
+    
+}
+
+function stockDraw(){
+
+}
