60a99516de795f6e974814444f0635adbc1866f3
trashbako <ienuenu@gmail.com>
2019-05-27 22:33:41 +0900

    fix js

--- docs/script/imgDraw.js
+++ docs/script/imgDraw.js
@@ -1,13 +1,18 @@
-
-//2Dコンテキストのオブジェクトを生成する
-var cvs = document.getElementById('cvs1');
-var ctx = cvs.getContext('2d');
-
-//画像オブジェクトを生成
-var img = new Image();
-img.src = "./img/v_sotai.png";
-
-//画像をcanvasに設定
-img.onload = function(){
-    ctx.drawImage(img, 0, 0, 600, 800);
+window.onload = function(){
+    imageDraw();
 }
+
+function imageDraw() {
+    //2Dコンテキストのオブジェクトを生成する
+    var cvs = document.getElementById('cvs1');
+    var ctx = cvs.getContext('2d');
+  
+    //画像オブジェクトを生成
+    var img = new Image();
+    img.src = "./img/v_sotai.png";
+  
+    //画像をcanvasに設定
+    img.onload = function(){
+      ctx.drawImage(img, 0, 0, 600, 800);
+    }
+  }
